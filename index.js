94c4e93d640e573ab316e7f540503d59da5aa4f3
majorblackc <charlesmk2018@gmail.com>
2022-05-28 02:23:45 +0300

    Indexes

---
+++ index.js
@@ -0,0 +1,28 @@
+// windows.alert("Hello")
+var x = document.getElementById("myFirstID");
+var y = document.getElementById("mySecondID");
+
+
+
+function myFunction(){
+    let name = "Charles";
+    console.error("Sorry this is only for my eyes");
+    return name
+} 
+
+function mySecondFunction(){
+    console.error("It's time to leave");
+    return "Sorry Goodbye"
+}
+
+
+const random_init = () => {
+    console.error("Here my function random_init");
+}
+
+
+x.innerHTML("<h1>Kisumu Web3 Dev Bootcamp</h1>");
+y.innerText("Welcome to Kisumu Web3 Dev Bootcamp at LakeHub Kisumu.");
+
+myFunction();
+mySecondFunction();
